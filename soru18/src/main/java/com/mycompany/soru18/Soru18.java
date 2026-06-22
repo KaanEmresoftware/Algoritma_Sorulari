43878b157e4c2c7cef6e907461e28037b63cb370
KaanEmresoftware <02250224078@ogr.inonu.edu.tr>
2026-06-22 10:31:51 +0300

    java2

---
+++ soru18/src/main/java/com/mycompany/soru18/Soru18.java
@@ -0,0 +1,20 @@
+
+
+package com.mycompany.soru18;
+
+
+public class Soru18 {
+
+    public static void main(String[] args) {
+        
+        Urun u1 = new Urun();
+        
+        u1.setAd("TV"); // ÖRNEK
+        u1.setBoy(12);  //ÖRNEK
+        u1.setEn(30);   // ÖRNEK
+        u1.setYuk(14);  //ÖRNEK
+        
+        u1.BilgiYazdir();
+        
+    }
+}
---
+++ soru18/src/main/java/com/mycompany/soru18/Urun.java
@@ -0,0 +1,38 @@
+
+package com.mycompany.soru18;
+
+
+public class Urun {
+    
+    private String ad;
+    private int en ;
+    private  int boy;
+    private int yukseklik;
+    
+    
+    public void setAd(String gad){
+        this.ad=gad;
+    }
+    
+    public void setEn(int gen){
+        this.en=gen;
+    }
+    
+    public void setBoy(int gboy){
+        this.boy=gboy;
+    }
+    
+    public void setYuk(int gyuk){
+        this.yukseklik=gyuk;
+    }
+    
+    
+    public void BilgiYazdir(){
+        System.out.println("Urun Ad:"+this.ad); 
+        System.out.println("Urun En:"+this.en);
+        System.out.println("Urun Boy:"+this.boy);
+        System.out.println("Urun Yukseklik:"+this.yukseklik);
+
+    }
+    
+}
